f3189b7e149365ea834186dab79becebf7a25320
Shin Juheon <juheon8794@gmail.com>
2025-09-25 11:09:15 +0900

    계산기 v1.0 소스 파일

---
+++ test/CalculatorTest.java
@@ -0,0 +1,33 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        Calculator calc = new Calculator();
+        calc.setNumbers(10, 20);
+        assertEquals(30,calc.add());
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtract() {
+        Calculator calc = new Calculator();
+        calc.setNumbers(30, 10);
+        assertEquals(20,calc.subtract());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+        Calculator calc = new Calculator();
+        calc.setNumbers(40, 50);
+        assertEquals(2000,calc.multiply());
+    }
+
+    @org.junit.jupiter.api.Test
+    void divide() {
+        Calculator calc = new Calculator();
+        calc.setNumbers(70, 10);
+        assertEquals(7,calc.divide());
+    }
+}
