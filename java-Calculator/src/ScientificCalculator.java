f9258df7e7d50a128b68f723932feebf4fc7fa1e
Shin Juheon <juheon8794@gmail.com>
2025-10-25 02:37:25 +0900

    출력 메서드 생성 및 코드 수정

--- java-Calculator/src/ScientificCalculator.java
+++ java-Calculator/src/ScientificCalculator.java
@@ -4,24 +4,36 @@ public class ScientificCalculator extends AdvancedCalculator {
 
     // 제곱 연산자 두 수로 제곱 완료
     // https://github.com/Shin-Juheon/Java-Calculator/issues/15
-    public int Power() {
-        int num = num1;
-        int exponent = num2;
+    private int num;
 
-        if (exponent == 0) {
+    public int Power() {
+        // 지수
+        int exp = num2;
+        // 밑값
+        num = num1;
+
+        // 음수 처리
+        if (exp < 0)
+        {
+            return -1;
+        }
+        if (exp == 0)
+        {
             result = 1;
             return result;
         }
         result = num;
-
-        for (int i = 1; i < exponent; i++) {
+        for (int i = 1; i < exp; i++) {
             setNumbers(result, num);
             result = multiply();
         }
         return result;
     }
+    // 제곱 연산자만 이용
+    public void PowerResult(String operator) {
+        System.out.println(num + " " + operator + " " + num2 + " = " + result);
+    }
 }
-
 
 // ---------- 수정 전 버전 ----------
 //    // 계산기 v3.0 개발
