0d4afd2442b7ab3484344293f0139aec3cc059ed
Shin Juheon <juheon8794@gmail.com>
2025-10-24 18:25:42 +0900

    계산기 v3.0 개선 완료

--- java-Calculator/src/ScientificCalculator.java
+++ java-Calculator/src/ScientificCalculator.java
@@ -1,16 +1,41 @@
-public class ScientificCalculator extends AdvancedCalculator{
-    // 계산기 v3.0 개발
-    // 이슈 https://github.com/Shin-Juheon/Java-Calculator/issues/9
+public class ScientificCalculator extends AdvancedCalculator {
+    // 계산기 v3.0 개선 버전
+    // 이슈 https://github.com/Shin-Juheon/Java-Calculator/issues/14
 
     // 제곱 연산자 두 수로 제곱 완료
-    // https://github.com/Shin-Juheon/Java-Calculator/issues/10
+    // https://github.com/Shin-Juheon/Java-Calculator/issues/15
     public int Power() {
         int num = num1;
         int exponent = num2;
-        result = 1;
-        for (int i = 1; i <= exponent; i++) {
-            result *= num;
+
+        if (exponent == 0) {
+            result = 1;
+            return result;
+        }
+        result = num;
+
+        for (int i = 1; i < exponent; i++) {
+            setNumbers(result, num);
+            result = multiply();
         }
         return result;
     }
 }
+
+
+// ---------- 수정 전 버전 ----------
+//    // 계산기 v3.0 개발
+//    // 이슈 https://github.com/Shin-Juheon/Java-Calculator/issues/9
+//
+//    // 제곱 연산자 두 수로 제곱 완료
+//    // https://github.com/Shin-Juheon/Java-Calculator/issues/10
+//    public int Power() {
+//        int num = num1;
+//        int exponent = num2;
+//        result = 1;
+//        for (int i = 1; i <= exponent; i++) {
+//            result *= num;
+//        }
+//        return result;
+//    }
+//}
